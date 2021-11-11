8a9b4bf208eb438506513039520f9c37c5751ceb
Rmdhn-20 <ramadhanart3@gmail.com>
2021-11-12 02:08:40 +0700

    Add file via upload

---
+++ message/donasi.js
@@ -0,0 +1,15 @@
+exports.dodo = (prefix) => {
+return`
+╭────────❲ *DONASI* ❳
+│
+│  Bantu Developer ngembangin bot ini
+│  dengan donasi seiklasnya 🙂
+│
+│  Dana : Scan code di pp bot
+│   atau 089618777587 A.n Eka Fajar Ramadhan
+│ 
+│ Makasih yang udah donasi
+│ dan bantu ngembangin bot ini
+│
+╰────────⏣`
+}
